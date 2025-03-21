7b247d4ebee9acdbc2f37e4581a2ebbb304932c5
Sanskriti <sanskritisri27@gmail.com>
2025-03-22 00:41:13 +0530

    added support for all 6 headings

--- src/components/HeadingDropdown.jsx
+++ src/components/HeadingDropdown.jsx
@@ -11,6 +11,10 @@ const headingOptions = [
   { label: "Paragraph", value: "paragraph" },
   { label: "Heading 1", value: "heading-1", level: 1 },
   { label: "Heading 2", value: "heading-2", level: 2 },
+  { label: "Heading 3", value: "heading-3", level: 3 },
+  { label: "Heading 4", value: "heading-4", level: 4 },
+  { label: "Heading 5", value: "heading-5", level: 5 },
+  { label: "Heading 6", value: "heading-6", level: 6 },
 ];
 
 const HeadingDropdown = ({ editor }) => {
